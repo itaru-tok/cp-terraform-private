fce538087ccb43ab2a9957e1ea896b440cc7e2ab
itaru-tok <itaruo93o@icloud.com>
2026-02-08 10:23:42 +0900

    import other 3 subnet

--- modules/aws/subnet/main.tf
+++ modules/aws/subnet/main.tf
@@ -7,3 +7,34 @@ resource "aws_subnet" "public_1c" {
     Name = "public-subnet-1c-${var.env}"
   }
 }
+
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.0.0.0/18"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1a-${var.env}"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.0.192.0/18"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-subnet-1c-${var.env}"
+  }
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.0.128.0/18"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-subnet-1a-${var.env}"
+  }
+}
+

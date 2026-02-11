421d78c61fa74bac76e1c4d759cd8ab2f256e39c
itaru-tok <itaruo93o@icloud.com>
2026-02-11 11:41:21 +0900

    import cp_db_migrator

--- modules/aws/iam_role/main.tf
+++ modules/aws/iam_role/main.tf
@@ -59,3 +59,23 @@ resource "aws_iam_role_policy_attachment" "cp_bastion" {
   role       = aws_iam_role.cp_bastion.name
   policy_arn = each.value
 }
+
+/************************************************************
+cp-db-migrator
+************************************************************/
+resource "aws_iam_role" "cp_db_migrator" {
+  name = "cp-db-migrator-${var.env}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}

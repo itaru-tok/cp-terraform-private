c3058c21b0c88f9b981355e2dbdef142e844167a
itaru-tok <itaruo93o@icloud.com>
2026-02-11 22:51:03 +0900

    import ECS auto scaling

--- modules/aws/ecs/main.tf
+++ modules/aws/ecs/main.tf
@@ -72,3 +72,54 @@ resource "aws_ecs_service" "slack_metrics_api" {
     ]
   }
 }
+
+resource "aws_appautoscaling_target" "slack_metrics_api" {
+  max_capacity       = 2
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.cloud_pratica_backend.name}/${aws_ecs_service.slack_metrics_api.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+  tags               = {}
+
+  lifecycle {
+    ignore_changes = [tags_all]
+  }
+
+  depends_on = [aws_ecs_service.slack_metrics_api]
+}
+
+resource "aws_appautoscaling_policy" "slack_metrics_api_cpu" {
+  name               = "target-tracking-cpu"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.slack_metrics_api.resource_id
+  scalable_dimension = aws_appautoscaling_target.slack_metrics_api.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.slack_metrics_api.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 60
+    target_value       = 70
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+  }
+}
+
+resource "aws_appautoscaling_policy" "slack_metrics_api_memory" {
+  name               = "target-tracking-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.slack_metrics_api.resource_id
+  scalable_dimension = aws_appautoscaling_target.slack_metrics_api.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.slack_metrics_api.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
+    target_value       = 70
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+  }
+}

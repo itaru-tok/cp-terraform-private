a57d552188c5947776e88eafc4cbf6c91c01bc54
itaru-tok <itaruo93o@icloud.com>
2026-02-11 12:05:12 +0900

    import ecs_task_execution

--- modules/aws/iam_role/main.tf
+++ modules/aws/iam_role/main.tf
@@ -108,3 +108,35 @@ resource "aws_iam_role_policy_attachment" "cp_nat" {
   role       = aws_iam_role.cp_nat.name
   policy_arn = each.value
 }
+
+/************************************************************
+ecs-task-execution
+************************************************************/
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecs-task-execution-${var.env}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  for_each = {
+    task_execution  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+    secrets_manager = aws_iam_policy.secrets_manager_read.arn
+    s3              = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    cloudwatch      = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+  }
+
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = each.value
+}

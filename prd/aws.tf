9d7548776374da8184b7666caacae6d64ba7ed71
itaru-tok <itaruo93o@icloud.com>
2026-02-14 16:38:36 +0900

    apply vpc, subnet, igw

---
+++ prd/aws.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source = "../modules/aws/vpc"
+  env    = local.env
+}
+
+module "subnet" {
+  source = "../modules/aws/subnet"
+  env    = local.env
+  vpc_id = module.vpc.id
+}
+
+module "igw" {
+  source = "../modules/aws/internet_gateway"
+  env    = local.env
+  vpc_id = module.vpc.id
+}

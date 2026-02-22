78c395c42f08f147da44f0d371793b71bf923e2c
itaru-tok <itaruo93o@icloud.com>
2026-02-23 00:46:09 +0900

    remove ecs task and service form tf

--- modules/aws/ecs/main.tf
+++ modules/aws/ecs/main.tf
@@ -22,61 +22,10 @@ resource "aws_ecs_cluster_capacity_providers" "cloud_pratica_backend" {
   capacity_providers = ["FARGATE", "FARGATE_SPOT"]
 }
 
-resource "aws_ecs_service" "slack_metrics_api" {
-  cluster                            = aws_ecs_cluster.cloud_pratica_backend.arn
-  name                               = var.slack_metrics_api.name
-  task_definition                    = var.slack_metrics_api.task_definition
-  desired_count                      = 1
-  enable_execute_command             = var.slack_metrics_api.enable_execute_command
-  enable_ecs_managed_tags            = true
-  health_check_grace_period_seconds  = 0
-  deployment_maximum_percent         = 200
-  deployment_minimum_healthy_percent = 100
-
-  capacity_provider_strategy {
-    base              = 0
-    capacity_provider = var.slack_metrics_api.capacity_provider
-    weight            = 1
-  }
-
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-
-  deployment_controller {
-    type = "ECS"
-  }
-
-  dynamic "load_balancer" {
-    for_each = var.slack_metrics_api.target_group_arn != null ? [1] : []
-    content {
-      container_name   = "api"
-      container_port   = 8080
-      target_group_arn = var.slack_metrics_api.target_group_arn
-    }
-  }
-
-  network_configuration {
-    assign_public_ip = false
-    security_groups  = var.slack_metrics_api.security_group_ids
-    subnets          = var.slack_metrics_api.subnet_ids
-  }
-
-  lifecycle {
-    ignore_changes = [
-      # MEMO: コスト削減で落とすタイミングがあるため
-      desired_count,
-      # MEMO: ecspressoからタスク定義のリビジョンを更新するため
-      task_definition,
-    ]
-  }
-}
-
 resource "aws_appautoscaling_target" "slack_metrics_api" {
   max_capacity       = 2
   min_capacity       = 1
-  resource_id        = "service/${aws_ecs_cluster.cloud_pratica_backend.name}/${aws_ecs_service.slack_metrics_api.name}"
+  resource_id        = "service/${aws_ecs_cluster.cloud_pratica_backend.name}/${var.slack_metrics_api.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
   tags               = {}
@@ -84,8 +33,6 @@ resource "aws_appautoscaling_target" "slack_metrics_api" {
   lifecycle {
     ignore_changes = [tags_all]
   }
-
-  depends_on = [aws_ecs_service.slack_metrics_api]
 }
 
 resource "aws_appautoscaling_policy" "slack_metrics_api_cpu" {

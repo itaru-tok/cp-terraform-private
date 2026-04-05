7426d5ba8bcc30ed8e5f6397ceafea1ad676b4a0
itaru-tok <itaruo93o@icloud.com>
2026-04-05 12:51:55 +0900

    apply argocd_github_oidc

--- modules/aws/secrets_manager/main.tf
+++ modules/aws/secrets_manager/main.tf
@@ -1,3 +1,19 @@
 resource "aws_secretsmanager_secret" "db_main_instance" {
   name = "db-main-instance-${var.env}"
 }
+
+# Argo CD（dex）GitHub OAuth: client_id / client_secret を JSON で保持。初回プレースホルダー後はコンソール等で更新し、Terraform は secret_string を変更しない
+resource "aws_secretsmanager_secret" "argocd_github_oidc" {
+  name = "argocd-github-oidc-${var.env}"
+}
+
+resource "aws_secretsmanager_secret_version" "argocd_github_oidc" {
+  secret_id = aws_secretsmanager_secret.argocd_github_oidc.id
+  secret_string = jsonencode({
+    client_id     = "REPLACE_ME"
+    client_secret = "REPLACE_ME"
+  })
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
--- modules/aws/secrets_manager/outputs.tf
+++ modules/aws/secrets_manager/outputs.tf
@@ -1,3 +1,7 @@
 output "arn_db_main_instance" {
   value = aws_secretsmanager_secret.db_main_instance.arn
 }
+
+output "arn_argocd_github_oidc" {
+  value = aws_secretsmanager_secret.argocd_github_oidc.arn
+}

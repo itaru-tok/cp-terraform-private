aed39557d1a481920f04768a5c670b52bfd65ae2
itaru-tok <itaruo93o@icloud.com>
2026-04-29 21:32:06 +0900

    remove import

--- stg/datadog.tf
+++ stg/datadog.tf
@@ -14,37 +14,3 @@ module "datadog_monitor" {
   slo_id_cost_provider_server_availability = module.datadog_slo.id_cost_provider_server_availability
   apm_view_url_cost_provider               = local.datadog_apm_view_url_cost_provider
 }
-
-# NOTE: 既存 SLO (401a8c3442d553fba7cc24df5da43b74) は sli_specification.count を含む新形式で
-# datadog provider 3.91 では import 時にパースエラーになる。一旦 import せず、Terraform で
-# 新しい SLO を作成し、SLO モニターのクエリは module 経由で新 SLO の ID を参照させる。
-# 既存 SLO は apply 後に Datadog UI から手動で削除する想定。
-# import {
-#   to = module.datadog_slo.datadog_service_level_objective.cost_provider_server_availability
-#   id = "401a8c3442d553fba7cc24df5da43b74"
-# }
-
-import {
-  to = module.datadog_monitor.datadog_monitor.cost_provider_server_error
-  id = "12776777"
-}
-
-import {
-  to = module.datadog_monitor.datadog_monitor.cost_provider_server_latency
-  id = "12777033"
-}
-
-import {
-  to = module.datadog_monitor.datadog_monitor.cost_provider_server_availability_short_burn_rate
-  id = "12777538"
-}
-
-import {
-  to = module.datadog_monitor.datadog_monitor.cost_provider_server_availability_medium_burn_rate
-  id = "12777569"
-}
-
-import {
-  to = module.datadog_monitor.datadog_monitor.cost_provider_server_availability_composite_burn_rate
-  id = "12777667"
-}
